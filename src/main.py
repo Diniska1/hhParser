9c9c971e4642e22d942f4f2cd8a88f21d2a99bc5
Denis <denispechenyy@gmail.com>
2025-12-07 17:57:11 +0300

    Added exceptions, fixed bug with resp_number

--- src/main.py
+++ src/main.py
@@ -20,13 +20,16 @@ async def main():
 
 
     url = 'https://hh.ru/search/vacancy?from=suggest_post&clickedSuggestId=b79c31ab-471f-4a35-95e9-a7fe296deb98&area=1&hhtmFrom=main&hhtmFromLabel=vacancy_search_line' + search + filter_experience
-    print(url)
+    # print(url)
 
     parser = Parse(url)
     db = DataBase()
 
-    task = asyncio.create_task(parser.get_all_vacancies(30,30))
-    vacs = await task
+    try:
+        vacs = await parser.get_all_vacancies(10,4)
+    except Exception as e:
+        print(f"Something went wrong: {e}")
+
     
     db.create_table()
     db.write_table(vacs)
--- src/methods/http.py
+++ src/methods/http.py
@@ -14,78 +14,83 @@ class Parse:
         self.parse_vacs = 0
     
     async def get_page_http(self, session, url):
-        # try:
-        async with session.get(url, headers=self.headers, timeout=10) as response:
-            if response.status == 200:
-                return await response.text()
+        try:
+            async with session.get(url, headers=self.headers, timeout=10) as response:
+                if response.status == 200:
+                    return await response.text()
+                else:
+                    print(f"Status {response.status} for {url}")
+                    return None
+        except Exception as e:
+            print(f"Error {e} while getting html of page: {url}")
+            
 
     async def data_from_vac(self, session, url):
         http = await self.get_page_http(session, url)
-        # response = requests.get(url, headers=self.headers)
 
-        # if response.status_code == 200:
-        soup = BeautifulSoup(http, 'html.parser')
-        s1 = "Опыт работы: "
-        s2 = "Формат работы: "
-        s3 = "Рабочие часы: "
-        s5 = "График: "
+        try:
+            soup = BeautifulSoup(http, 'html.parser')
+            s1 = "Опыт работы: "
+            s2 = "Формат работы: "
+            s3 = "Рабочие часы: "
+            s5 = "График: "
 
-        title_and_wage = soup.find('div', {'class': "vacancy-title"}).text
-        title = soup.find('h1', {'data-qa': "vacancy-title"}).text
-        wage = title_and_wage[len(title):]
+            title_and_wage = soup.find('div', {'class': "vacancy-title"}).text
+            title = soup.find('h1', {'data-qa': "vacancy-title"}).text
+            wage = title_and_wage[len(title):]
+            
+            exp = soup.find('p', {"data-qa": "work-experience-text"}).text[len(s1):]
+            common_empl = soup.find("div", {"data-qa":"common-employment-text"}).text
+            work_hours = soup.find('div', {"data-qa":"working-hours-text"}).text[len(s3):]
+            format_work = soup.find('p', {"data-qa":"work-formats-text"})
+            work_schedule = soup.find("p", {"data-qa":"work-schedule-by-days-text"}).text[len(s5):]
+            company_name = soup.find("a", {"data-qa": "vacancy-company-name"}).text
+
+            resp = soup.find('div', {"class":"noprint"}).text
+            resp_number = ''.join(filter(str.isdigit, resp))
+            resp_number = "1" if len(resp_number) == 0 else resp_number
+
+            format_work = "Не указан" if format_work == None else format_work.text[len(s2): ]
+
+            res = VacData(url, title, wage, exp, common_empl, work_hours, format_work, resp_number, work_schedule, company_name)
         
-        exp = soup.find('p', {"data-qa": "work-experience-text"}).text[len(s1):]
-        common_empl = soup.find("div", {"data-qa":"common-employment-text"}).text
-        work_hours = soup.find('div', {"data-qa":"working-hours-text"}).text[len(s3):]
-        format_work = soup.find('p', {"data-qa":"work-formats-text"})
-        work_schedule = soup.find("p", {"data-qa":"work-schedule-by-days-text"}).text[len(s5):]
-        company_name = soup.find("a", {"data-qa": "vacancy-company-name"}).text
-
-        resp = soup.find('div', {"class":"noprint"}).text
-        resp_number = ''.join(filter(str.isdigit, resp))
-
-        format_work = "Не указан" if format_work == None else format_work.text[len(s2): ]
-
-        res = VacData(url, title, wage, exp, common_empl, work_hours, format_work, resp_number, work_schedule, company_name)
+        except Exception as e:
+            print(f"Error {e} while parsing page {url}")
 
         return res
 
         
     async def get_all_vacancies_on_page(self, session, url, num_vacancies):
         
-        http = await self.get_page_http(session, url)
+        try:
+            http = await self.get_page_http(session, url)
 
-        vacs = []
-        tasks = []
-        
-        soup = BeautifulSoup(http, 'html.parser')
-        vacs_soup = soup.find_all("div", {"class":"magritte-redesign"})
-        
-        if self.is_first_page:
-            all_vacs = soup.find("div", {"data-qa":"title-container"}).text
-            print(f"{all_vacs}")
-            num_all_vacs = ''.join(filter(str.isdigit, all_vacs))
-            self.parse_vacs = min(self.parse_vacs, int(num_all_vacs))
-            self.is_first_page = 0
-        
-        for vac in vacs_soup[1:num_vacancies+1]:
-            vac_ref = vac.find("a", {"data-qa" : "serp-item__title"}).attrs['href']
-            task = asyncio.create_task(
-                self.data_from_vac(session, vac_ref)
-            )
-            tasks.append(task)
+            vacs = []
+            tasks = []
+            
+            soup = BeautifulSoup(http, 'html.parser')
+            vacs_soup = soup.find_all("div", {"class":"magritte-redesign"})
+            
+            if self.is_first_page:
+                all_vacs = soup.find("div", {"data-qa":"title-container"}).text
+                print(f"{all_vacs}")
+                num_all_vacs = ''.join(filter(str.isdigit, all_vacs))
+                self.parse_vacs = min(self.parse_vacs, int(num_all_vacs))
+                self.is_first_page = 0
+            
+            for vac in vacs_soup[1:num_vacancies+1]:
+                vac_ref = vac.find("a", {"data-qa" : "serp-item__title"}).attrs['href']
+                task = asyncio.create_task(
+                    self.data_from_vac(session, vac_ref)
+                )
+                tasks.append(task)
 
-        results = await asyncio.gather(*tasks, return_exceptions=True)
+            results = await asyncio.gather(*tasks, return_exceptions=True)
 
-        for result in results:
-            # if isinstance(result, Exception):
-            #     print(f"Error in page parsing: {result}")
-            # elif result:
-            vacs.append(result)
-                
-        # else:
-        #     print("ERROR: response.status_code != 200")
-        #     exit(1)
+            for result in results:
+                vacs.append(result)
+        except Exception as e:
+            print(f"Error {e} while parsing main page: {url}")
         
         return vacs
         
